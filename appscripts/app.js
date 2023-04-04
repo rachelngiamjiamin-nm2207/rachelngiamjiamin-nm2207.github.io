64f6b4abe82607b4bbd5e6e1cf3e81b90d09e714
rachelngiamjiamin-nm2207 <125366339+rachelngiamjiamin-nm2207@users.noreply.github.com>
2023-04-04 23:05:48 +0800

    Updated app.js

--- appscripts/app.js
+++ appscripts/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){ //executes once the page loads
     let colors = ["red","blue","green","yellow","purple","orange"];
     codepegArray = [];
     answerCode = [];
-    let round = 0;
+    let roundRow = 6;
 
     //randomizing an answer code
     for (i=0; i<4; i++){
@@ -15,12 +15,11 @@ $(document).ready(function(){ //executes once the page loads
     console.log("answer code: "+answerCode);
     colorsPresent = [];
     
-    //checks for colors present in answer code
     for (i=0;i<4;i++){
         console.log(colorsPresent.includes(answerCode[i].color));
         if (colorsPresent.includes(answerCode[i].color) == false){
             colorsPresent.push(answerCode[i].color);
-            //console.log("colorspresent: "+colorsPresent);
+            console.log("colorspresent: "+colorsPresent);
         };
     };
 
@@ -35,63 +34,129 @@ $(document).ready(function(){ //executes once the page loads
     
     codepegCol = document.getElementById("codepegs");
 
-    //call the function & add the HTML code 
     for(let i=0; i<7; i++){
+        codepegCol.innerHTML += "<div class = \"codeRows\" id = \"codeRow_"+ i +"\" ></div>"
+        codepegRow = document.getElementById("codeRow_"+i);
         newCodepegRow = [];
         for (let j=0; j<4; j++){
             newCodepeg = new codepeg("grey",i,j);
-            codepegCol.innerHTML += "<span class = \"dot_codepeg\" id = \"codepeg_" +i +"_"+j+"\" style=\"background-color: grey\"></span>";
             newCodepegRow.push(newCodepeg);
+            codepegRow.innerHTML += "<div class = \"dot_codepeg\" id = \"codepeg_" +i +"_"+j+"\" style=\"background-color: grey\"></div>";
         };
         codepegArray.push(newCodepegRow);
-        codepegCol.innerHTML += "<br>";
+        codepegRow.innerHTML += "<br></div>";
     };
-    // console.log(codepegCol.innerHTML);
-    // console.log("printing codepegArray");
-    // console.log(codepegArray);
-    // console.log("codepegsHTML: "+col.innerHTML);
 
-    //creating empty hint pegs
+    console.log(document.getElementById("codepegs").innerHTML);
+
+    //version i'm editing on
     hintpegCol = document.getElementById("hintpegs");
+
     for(let i=0; i<7; i++){
+        hintpegCol.innerHTML += "<div class = \"hintRows\" id = \"hintRow_"+ i +"\" ></div>"
+        hintpegRow = document.getElementById("hintRow_"+i);
         for (let j=0; j<4; j++){
-            hintpegCol.innerHTML += "<span class = \"dot_hintpeg\" id = \"hintpeg_" +i +"_"+j+"\" style=\"background-color: grey\"></span>";
+            hintpegRow.innerHTML += "<div class = \"dot_hintpeg\" id = \"hintpeg_" +i +"_"+j+"\" style=\"background-color: grey\"></div>";
+            if (j==1){
+                hintpegRow.innerHTML+="<br>";
+            }
         };
-        hintpegCol.innerHTML += "<br>";
+        hintpegRow.innerHTML += "<br></div>";
     };
+
+    console.log(document.getElementById("hintpegs").innerHTML);
     
     //default
     currentColor = "grey";
 
     colorDropper = function(colorSelected){
-        //update the current color variable
         if (currentColor != colorSelected){
             currentColor = colorSelected;
         }
-        //add event listeners to the current codepegs of this round
-        for (j=0;j<4;j++){
-            document.getElementById("codepeg_"+round+"_"+j).addEventListener("click",changeColor(currentColor,j));
+        row = [0,1,2,3];
+        for (const pegCol of row){
+            curID = "codepeg_"+roundRow+"_"+pegCol;
+            document.getElementById("codepeg_"+roundRow+"_"+pegCol).addEventListener("click",function(){changeColor(currentColor,pegCol);});
         }
     }
 
-    //change the color of the codepeg selected
     changeColor = function(colorSelected,pegCol){
-        //finding which OBJECT the codepeg is
-        for(i=0;i<codepegArray.length;i++){
-            codepegObj = codepegArray[i];
-            if (codepegObj.codepegRow == round && codepegObj.codepegCol == pegCol){
-                //change the style attribute in the codepeg html
-                codepegElement = document.getElementById("codepeg_"+round+"_"+pegCol);
-                codepegElement.innerHTML = "<span class = \"dot_codepeg\" id = \"codepeg_" +round +"_"+pegCol+"\" style=\"background-color: "+colorSelected+"\"></span>";
-                selectedCodepeg = codepegArray[i];
-                break;
-            }
-        }
-        codepegElement.color = colorSelected;
+        //changing the HTML style attribute
+        codepegElement = document.getElementById("codepeg_"+roundRow+"_"+pegCol);
+        codepegElement.innerHTML = "<div class = \"dot_codepeg\" id = \"codepeg_" +roundRow +"_"+pegCol+"\" style=\"background-color: "+colorSelected+"\"></div>";
+        
+        //changing the "color" attribute of the OBJECT
+        curObjRow = codepegArray[roundRow];
+        const tarCodepeg = curObjRow.find(item => item.codepegCol === pegCol);
+        tarCodepeg.color = colorSelected;
     }
 
-    for (i=0;i<6;i++){
-        document.getElementById(colors[i]).addEventListener("click",colorDropper(colors[i]));
-        //possible to change cursor to a color dropper??
+    for (const curColor of colors){
+        document.getElementById(curColor).addEventListener("click",function(){colorDropper(curColor);});
     }
+
+    checkAnswer = function(){
+        currentRowCode = codepegArray[roundRow];
+        userAns = [];
+        const correctAns = [...answerCode];
+        console.log("for this round, the answer code starts at: "+correctAns);
+        for (j=0;j<4;j++){
+            userAns.push(currentRowCode[j].color);
+            console.log(userAns);
+        }
+        blackHints = 0;
+        whiteHints = 0;
+        if (userAns.includes("grey")){
+            window.alert("Round voided.");
+        }
+        else {
+            for (i=0;i<4;i++){
+                if (userAns[i] == correctAns[i] && userAns[i]!="NIL"){
+                    console.log("checking for black hints: ")
+                    console.log("user ans: "+userAns[i]);
+                    console.log("user ans: "+correctAns[i]);
+                    blackHints += 1;
+                    userAns[i] = "NIL";
+                    correctAns[i] = "NIL";
+                }
+            }
+            for (k=0;k<4;k++){
+                if (userAns[k]!="NIL"){
+                    for (m=0;m<4;m++){
+                        if (userAns[k] == correctAns[m]){
+                            whiteHints += 1;
+                            userAns[k] = "NIL";
+                            correctAns[m] = "NIL";
+                            break;
+                        }
+                    }
+                }
+            }      
+        }
+        // console.log("end of function");
+        displayHints(blackHints,whiteHints,roundRow);
+        if (blackHints == 4){
+            window.alert("CONGRATULATIONS! You have cracked the code!");
+        }
+        roundRow -= 1;
+    }
+
+    displayHints = function(blackHints,whiteHints,roundRow){
+        bHints = blackHints*1;
+        wHints = whiteHints*1;
+        numOfHints = bHints + wHints;
+        
+        for(i=0;i<bHints;i++){
+            // console.log("i loop is called: "+i);
+            curHintpegBlack = document.getElementById("hintpeg_"+roundRow+"_"+i);
+            curHintpegBlack.innerHTML = "<div class = \"dot_hintpeg\" id = \"hintpeg_" +roundRow +"_"+i+"\" style=\"background-color: black\"></div>";
+        }
+
+        for(j=bHints;j<numOfHints;j++){
+            curHintpegBlack = document.getElementById("hintpeg_"+roundRow+"_"+j);
+            curHintpegBlack.innerHTML = "<div class = \"dot_hintpeg\" id = \"hintpeg_" +roundRow +"_"+j+"\" style=\"background-color: white\"></div>";
+        }
+    }
+
+    document.getElementById("checkButton").addEventListener("click", function(){checkAnswer();});
 });
